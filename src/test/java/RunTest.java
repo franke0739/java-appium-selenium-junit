c88f831637bc92323251765538b74bfdf0eda535
Frank Tam <frank@hi.com>
2022-09-09 18:10:28 +0800

    sample 2

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,4 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,18 +13,28 @@ import java.time.Duration;
 
 
 public class RunTest {
-
-    @Test
-    public void testBuyIphone14Pro() throws InterruptedException {
+    WebDriver webDriver;
+    WebDriverWait webDriverWait;
+    @Before
+    public void beforeTest(){
         // Launch Chrome Browser
         WebDriverManager.chromedriver().setup();
-        WebDriver webDriver = new ChromeDriver();
-
-        // Navigate to Apple official website
-        webDriver.get("https://www.apple.com/");
+        webDriver = new ChromeDriver();
 
         // Create Webdriver Wait Object
-        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+        webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+    }
+
+    @After
+    public void afterTest(){
+        // Close Chrome Browser
+        webDriver.quit();
+    }
+
+    @Test
+    public void testViewPriceOfIphone14Pro() throws InterruptedException {
+        // Navigate to Apple official website
+        webDriver.get("https://www.apple.com/");
 
         // Assign iphoneLinkLocator locator
         By iphoneLinkLocator = By.className("ac-gn-link-iphone");
@@ -59,6 +71,54 @@ public class RunTest {
 
         // Click viewPricingButton
         viewPricingButton.click();
+
+        // Sleep 5 seconds
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testSearchIphone14ProMax() throws InterruptedException{
+        // Navigate to Apple official website
+        webDriver.get("https://www.apple.com/");
+
+        // Assign searchButtonLocator locator
+        By searchButtonLocator = By.id("ac-gn-link-search");
+
+        // Wait searchButton to presence
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(searchButtonLocator));
+
+        // Find searchButton
+        WebElement searchButton = webDriver.findElement(searchButtonLocator);
+
+        // Click searchButton
+        searchButton.click();
+
+        // Assign searchInputLocator locator
+        By searchInputLocator = By.id("ac-gn-searchform-input");
+
+        // Wait searchInput to presence
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(searchInputLocator));
+
+        // Find searchInput
+        WebElement searchInput = webDriver.findElement(searchInputLocator);
+
+        // Send text to searchInput
+        searchInput.sendKeys("iphone 14 Pro Max");
+
+        // Sleep 5 seconds, so you can see the text entered
+        Thread.sleep(5000);
+
+        // Assign submitSearchButtonLocator locator
+        By submitSearchButtonLocator = By.id("ac-gn-searchform-submit");
+
+        // Wait searchInput to presence
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(submitSearchButtonLocator));
+
+        // Find searchInput
+        WebElement submitSearchButton = webDriver.findElement(submitSearchButtonLocator);
+
+        // Click submitSearchButton
+        submitSearchButton.click();
 
         // Sleep 5 seconds
         Thread.sleep(5000);
